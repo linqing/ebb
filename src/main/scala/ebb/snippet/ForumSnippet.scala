b73c7eee5e7827410eea44577269ad098a009067
Freewind <nowind_lee@qq.com>
2011-11-07 15:35:01 +0800

    add more field values

--- src/main/scala/ebb/snippet/ForumSnippet.scala
+++ src/main/scala/ebb/snippet/ForumSnippet.scala
@@ -5,14 +5,30 @@ import ebb.model.Models._
 import org.squeryl._
 import PrimitiveTypeMode._
 import net.liftweb.util.Helpers._
+import java.util._ 
+import java.text._
 
 object ForumSnippet {
   def forumlist: CssSel =
     "*" #> (for (cat <- cats) yield {
       ".t-cat_header" #> ("@cat_name" #> cat.name) &
-        ".t-forum" #> (for (f <- cat.forums) yield {
-          ".forumname *" #> f.name &
-            ".forumdescr *" #> f.descr
-        })
+      ".t-forum" #> (
+        for {
+          f <- cat.forums
+	      lastTopic <- topics.lookup(f.last_topic_id)
+	      val lastPost = posts.lookup(lastTopic.last_post_id).get
+	      val lastPoster = members.lookup(lastPost.poster_id).get
+	      val lastTopicTitle = (if(lastTopic.count_replies>1) "Re: " else "") + lastTopic.topic_title
+	      val lastTopicUrl = "?????????????" //TODO
+	      val lastPosterUrl = "?????????????" //TODO
+        } yield {
+        ".forumname *" #> f.name &
+        ".forumdescr *" #> f.descr &
+        ".total_topics" #> f.topics &
+        ".total_posts" #> f.posts &
+        ".latest_post *" #> <a href={lastTopicUrl}>{lastTopicTitle}</a> &
+        ".by_author *" #> <span>By: <a href={lastPosterUrl}>{lastPoster.name}</a></span> &
+        ".on_date *" #> ("On: " + new SimpleDateFormat().format(lastPost.post_time))
+      })
     })
 }
