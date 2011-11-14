de66fa96a67f90b0674285577d172ab65df452e1
Freewind <nowind_lee@qq.com>
2011-11-14 20:42:21 +0800

    将for中的赋值放到yield中了

--- src/main/scala/ebb/snippet/ForumSnippet.scala
+++ src/main/scala/ebb/snippet/ForumSnippet.scala
@@ -1,47 +1,32 @@
-package ebb.snippet
+package ebb
+package snippet
 
-import net.liftweb.http._
 import net.liftweb.util._
 import ebb.model.Models._
 import org.squeryl._
 import PrimitiveTypeMode._
 import net.liftweb.util.Helpers._
-import java.util._
+import java.util._ 
 import java.text._
 
 object ForumSnippet {
   def forumlist: CssSel =
     "*" #> (for (cat <- cats) yield {
       ".t-cat_header" #> ("@cat_name" #> cat.name) &
-        ".t-forum" #> (for {
-          f <- cat.forums
-          lastTopic <- topics.lookup(f.last_topic_id)
-          val lastPost = posts.lookup(lastTopic.last_post_id).get
-          val lastPoster = members.lookup(lastPost.poster_id).get
-          val lastTopicTitle = (if (lastTopic.count_replies > 1) "Re: " else "") + lastTopic.topic_title
-          val lastTopicUrl = "?????????????" //TODO
-          val lastPosterUrl = "?????????????" //TODO
-        } yield {
-          ".forumname *" #> f.name &
-            ".forumdescr *" #> <a href={"/forum?id=" + f.id}>{ f.descr }</a> &
-            ".total_topics" #> f.topics &
-            ".total_posts" #> f.posts &
-            ".latest_post *" #> <a href={ lastTopicUrl }>{ lastTopicTitle }</a> &
-            ".by_author *" #> <span>By:<a href={ lastPosterUrl }>{ lastPoster.name }</a></span> &
-            ".on_date *" #> ("On: " + new SimpleDateFormat().format(lastPost.post_time))
-        })
+      ".t-forum" #> ( for {
+        f <- cat.forums
+        lastTopic <- topics.lookup(f.last_topic_id)
+      } yield {
+        val lastPost = posts.lookup(lastTopic.last_post_id).get
+        val lastPoster = members.lookup(lastPost.poster_id).get
+        val lastTopicTitle = (if(lastTopic.count_replies>1) "Re: " else "") + lastTopic.topic_title
+        ".forumname *" #> f.name &
+        ".forumdescr *" #> f.descr &
+        ".total_topics" #> f.topics &
+        ".total_posts" #> f.posts &
+        ".latest_post *" #> ("@latest_post" #> lastTopicTitle) &
+        ".by_author *" #> ("@by_author" #> {lastPoster.name}) &
+        ".on_date *" #> ("@on_date" #> new SimpleDateFormat().format(lastPost.post_time))
+      })
     })
-    
-    
-	def show:CssSel = {
-	  "*" #> ( for {
-	    forumId <- S.param("id")
-	    forum <- forums.lookup(forumId.toLong)
-	  } yield {
-	  "@topic *" #> (
-	      "@name *" #> forum.name &
-	      "@posts *" #> forum.posts
-	      )  
-	  })
-	}
 }
