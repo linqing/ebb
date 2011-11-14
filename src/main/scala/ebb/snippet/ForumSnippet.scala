5832bf6bcb1b10d914bab99a4e7efff53ecc9def
Freewind <nowind_lee@qq.com>
2011-11-14 21:28:25 +0800

    错误地覆盖了linqing的代码，重新加上

--- src/main/scala/ebb/snippet/ForumSnippet.scala
+++ src/main/scala/ebb/snippet/ForumSnippet.scala
@@ -1,32 +1,45 @@
 package ebb
 package snippet
 
+import net.liftweb.http._
 import net.liftweb.util._
 import ebb.model.Models._
 import org.squeryl._
 import PrimitiveTypeMode._
 import net.liftweb.util.Helpers._
-import java.util._ 
+import java.util._
 import java.text._
 
 object ForumSnippet {
   def forumlist: CssSel =
     "*" #> (for (cat <- cats) yield {
       ".t-cat_header" #> ("@cat_name" #> cat.name) &
-      ".t-forum" #> ( for {
-        f <- cat.forums
-        lastTopic <- topics.lookup(f.last_topic_id)
-      } yield {
-        val lastPost = posts.lookup(lastTopic.last_post_id).get
-        val lastPoster = members.lookup(lastPost.poster_id).get
-        val lastTopicTitle = (if(lastTopic.count_replies>1) "Re: " else "") + lastTopic.topic_title
-        ".forumname *" #> f.name &
-        ".forumdescr *" #> f.descr &
-        ".total_topics" #> f.topics &
-        ".total_posts" #> f.posts &
-        ".latest_post *" #> ("@latest_post" #> lastTopicTitle) &
-        ".by_author *" #> ("@by_author" #> {lastPoster.name}) &
-        ".on_date *" #> ("@on_date" #> new SimpleDateFormat().format(lastPost.post_time))
-      })
+        ".t-forum" #> (for {
+          f <- cat.forums
+          lastTopic <- topics.lookup(f.last_topic_id)
+        } yield {
+          val lastPost = posts.lookup(lastTopic.last_post_id).get
+          val lastPoster = members.lookup(lastPost.poster_id).get
+          val lastTopicTitle = (if (lastTopic.count_replies > 1) "Re: " else "") + lastTopic.topic_title
+          ".forumname *" #> f.name &
+            ".forumdescr *" #> f.descr &
+            ".total_topics" #> f.topics &
+            ".total_posts" #> f.posts &
+            ".latest_post *" #> ("@latest_post" #> lastTopicTitle) &
+            ".by_author *" #> ("@by_author" #> { lastPoster.name }) &
+            ".on_date *" #> ("@on_date" #> new SimpleDateFormat().format(lastPost.post_time))
+        })
     })
+
+  def show: CssSel = {
+    "*" #> (for {
+      forumId <- S.param("id")
+      forum <- forums.lookup(forumId.toLong)
+    } yield {
+      "@topic *" #> {
+        "@name *" #> forum.name &
+          "@posts *" #> forum.posts
+      }
+    })
+  }
 }
